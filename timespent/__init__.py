cceb64672c87f3b0c93315171c0f55a59ddde02a
josephggd <70484954+josephggd@users.noreply.github.com>
2024-09-09 16:40:48 -0500

    Delete timespent directory

--- timespent/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['TimeSpent', 'FilterType', 'Filter', 'CommitFilter', 'CommentFilter', 'DurationException', 'WorkSession']
-
-from .timespent import TimeSpent, WorkSession, DurationException
-from .commit import CommitFilter, CommentFilter, FilterType, Filter
