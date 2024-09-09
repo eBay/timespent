ec64ed5fff0d612518081f8f46bccb546606941b
josephggd <70484954+josephggd@users.noreply.github.com>
2024-09-09 16:40:56 -0500

    Delete test_timespent directory

--- test_timespent/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['TimeSpent','FilterType','Filter','CommitFilter','CommentFilter','GitQuery','DurationException','WorkSession']
-
-from timespent import TimeSpent, WorkSession, DurationException
-from commit import CommitFilter, CommentFilter, FilterType, Filter
-from gitquery import GitQuery
